ede702ea63c9e223f5e9394f5251865eec5c87e4
David Winterbottom <david.winterbottom@gmail.com>
2012-04-17 14:32:31 +0100

    Added pretty display of key-value data from PP

--- paypal/express/admin.py
+++ paypal/express/admin.py
@@ -5,6 +5,22 @@ from paypal.express import models
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['method', 'amount', 'currency', 'correlation_id', 'ack', 'token',
                     'date_created']
+    readonly_fields = [
+        'method',
+        'version',
+        'amount',
+        'currency',
+        'ack',
+        'correlation_id',
+        'token',
+        'error_code',
+        'error_message',
+        'raw_request',
+        'raw_response',
+        'response_time',
+        'date_created',
+        'request', 
+        'response']
 
 
 admin.site.register(models.Transaction, TransactionAdmin)
