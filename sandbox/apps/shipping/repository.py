d91f95c29dcf96d03b3e0c1f734425a134285563
David Winterbottom <david.winterbottom@gmail.com>
2012-05-03 22:46:30 +0100

    Added explanatory comments

--- sandbox/apps/shipping/repository.py
+++ sandbox/apps/shipping/repository.py
@@ -1,9 +1,15 @@
 from decimal import Decimal as D
+
 from oscar.apps.shipping.methods import Free, FixedPrice
 from oscar.apps.shipping.repository import Repository as CoreRepository
 
 
 class Repository(CoreRepository):
+    """
+    This class is included so that there is a choice of shipping methods.
+    Oscar's default behaviour is to only have one which means you can't test the
+    shipping features of PayPal.
+    """
 
     def get_shipping_methods(self, user, basket, shipping_addr=None, **kwargs):
         methods = [Free(), FixedPrice(D('10.00')), FixedPrice(D('20.00'))]
--- sandbox/urls.py
+++ sandbox/urls.py
@@ -11,16 +11,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+    # Include the below line to enable PayPal integration
     (r'^checkout/paypal/', include('paypal.express.urls')),
     (r'', include(shop.urls)),
 )
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    if settings.DEBUG:
-        urlpatterns += staticfiles_urlpatterns()
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-        urlpatterns += patterns('',
-            url(r'^404$', TemplateView.as_view(template_name='404.html')),
-            url(r'^500$', TemplateView.as_view(template_name='500.html')),
-        )
+    urlpatterns += patterns('',
+        url(r'^404$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500$', TemplateView.as_view(template_name='500.html')),
+    )
