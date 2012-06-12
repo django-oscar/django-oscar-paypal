b285ec851f0c1ad4c4e7c03a62126482120383cb
David Winterbottom <david.winterbottom@gmail.com>
2012-06-12 22:16:30 +0100

    Bump revision number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-oscar-paypal',
-      version='0.1',
+      version='0.1.1',
       url='https://github.com/tangentlabs/django-oscar-paypal',
       author="David Winterbottom",
       author_email="david.winterbottom@tangentlabs.co.uk",
