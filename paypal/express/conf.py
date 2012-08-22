cb93f1cd9a240b3299aeaa788c69c51f0b3be97f
David Winterbottom <david.winterbottom@gmail.com>
2012-08-22 21:00:27 +0100

    Remove conf files

--- paypal/express/conf.py
+++
@@ -1,4 +0,0 @@
-ENDPOINT = "https://api-3t.paypal.com/nvp"
-SANDBOX_ENDPOINT = "https://api-3t.sandbox.paypal.com/nvp"
-EXPRESS_ENDPOINT = "https://www.paypal.com/webscr?cmd=_express-checkout&%s"
-SANDBOX_EXPRESS_ENDPOINT = "https://www.sandbox.paypal.com/webscr?cmd=_express-checkout&%s"
--- paypal/payflow/conf.py
+++
@@ -1,3 +0,0 @@
-# Taken from the PayFlow PDF guide (see docs/ folder)
-TEST_HOST = 'pilot-payflowpro.paypal.com'
-LIVE_HOST = 'payflowpro.paypal.com'
