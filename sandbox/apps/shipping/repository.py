0d58c2ffc8ec6afc353a242f942f668b0b7f362c
David Winterbottom <david.winterbottom@gmail.com>
2013-04-10 12:10:22 +0100

    Correct shipping repository method calls
    Oscar 0.5 has renamed the repository methods.

--- sandbox/apps/shipping/repository.py
+++ sandbox/apps/shipping/repository.py
@@ -7,10 +7,10 @@ from oscar.apps.shipping.repository import Repository as CoreRepository
 class Repository(CoreRepository):
     """
     This class is included so that there is a choice of shipping methods.
-    Oscar's default behaviour is to only have one which means you can't test the
-    shipping features of PayPal.
+    Oscar's default behaviour is to only have one which means you can't test
+    the shipping features of PayPal.
     """
 
     def get_shipping_methods(self, user, basket, shipping_addr=None, **kwargs):
         methods = [Free(), FixedPrice(D('10.00')), FixedPrice(D('20.00'))]
-        return self.add_basket_to_methods(basket, methods)
+        return self.prime_methods(basket, methods)
