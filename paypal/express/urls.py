282454355589042fe79738634cb841e3dcfc764a
David Winterbottom <david.winterbottom@gmail.com>
2013-06-13 12:30:11 +0100

    Submit the frozen basket within success view
    This involves some trickyness with the templates as the default
    preview.html wants to render the open basket (which is injected into the
    template context via the basket middleware).  We duplicate some of the
    basket template so we can render an alternative basket (the frozen one).

--- paypal/express/urls.py
+++ paypal/express/urls.py
@@ -7,11 +7,12 @@ from paypal.express import views
 urlpatterns = patterns('',
     # Views for normal flow that starts on the basket page
     url(r'^redirect/', views.RedirectView.as_view(), name='paypal-redirect'),
-    url(r'^preview/', views.SuccessResponseView.as_view(preview=True),
+    url(r'^preview/(?P<basket_id>\d+)/$',
+        views.SuccessResponseView.as_view(preview=True),
         name='paypal-success-response'),
-    url(r'^cancel/(?P<basket_id>\d+)/', views.CancelResponseView.as_view(),
+    url(r'^cancel/(?P<basket_id>\d+)/$', views.CancelResponseView.as_view(),
         name='paypal-cancel-response'),
-    url(r'^place-order/', views.SuccessResponseView.as_view(),
+    url(r'^place-order/(?P<basket_id>\d+)/$', views.SuccessResponseView.as_view(),
         name='paypal-place-order'),
     # Callback for getting shipping options for a specific basket
     url(r'^shipping-options/(?P<basket_id>\d+)/',
