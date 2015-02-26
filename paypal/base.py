django-oscar/django-oscar-paypal

commit b5eaa0def9f082823fce5bc70ab5c688848f6c5c

Fix Paypal response for Python3.x
The mocked `response.content` was returning a str object in python2.7 and a unicode string in python3.x, but it should return  a byte string.
Tests were passing even if byte stings were not handled correctly in python3.

Use `response.text` instead of `response.content` (it returns an unicode object).
Use unicode string when `requests` app is involved with `from __future__ import unicode_literals`

paypal/base.py
@@ -1,4 +1,4 @@
-from django.utils.encoding import force_text
+from __future__ import unicode_literals
 from django.utils.six.moves.urllib.parse import parse_qs
 from django.utils.translation import ugettext_lazy as _
 
@@ -47,4 +47,4 @@ class ResponseModel(models.Model):
 
     def value(self, key, default=None):
         ctx = self.context
-        return force_text(ctx[key][0]) if key in ctx else default
+        return ctx[key][0] if key in ctx else default

paypal/gateway.py
@@ -1,7 +1,7 @@
+from __future__ import unicode_literals
 import requests
 import time
 
-from django.utils.encoding import force_text
 from django.utils.http import urlencode
 from django.utils.six.moves.urllib.parse import parse_qs
 
@@ -26,12 +26,12 @@ def post(url, params):
 
     # Convert response into a simple key-value format
     pairs = {}
-    for key, values in parse_qs(response.content).items():
-        pairs[force_text(key)] = force_text(values[0])
+    for key, values in parse_qs(response.text).items():
+        pairs[key] = values[0]
 
     # Add audit information
     pairs['_raw_request'] = payload
-    pairs['_raw_response'] = response.content
+    pairs['_raw_response'] = response.text
     pairs['_response_time'] = (time.time() - start_time) * 1000.0
 
     return pairs

paypal/express/gateway.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import logging
 from decimal import Decimal as D
 

paypal/payflow/gateway.py
@@ -2,6 +2,7 @@
 Gateway module - this module should be ignorant of Oscar and could be used in a
 non-Oscar project.  All Oscar-related functionality should be in the facade.
 """
+from __future__ import unicode_literals
 import logging
 
 from django.conf import settings

paypal/express/facade.py
@@ -1,6 +1,7 @@
 """
 Responsible for briding between Oscar and the PayPal gateway
 """
+from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import Site
 from django.conf import settings

paypal/express/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import re
 
 from django.db import models

paypal/express/views.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from decimal import Decimal as D
 import logging
 

paypal/payflow/codes.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 # Make strings collectable with gettext tools, but don't trnslate them here:
 _ = lambda x: x
 

commit 8e6851fdcb7a725c4e00f77a9081ec45e6cf6573

Fix: Python 2 standard urllib is not unicode friendly.
`parse_qs` parses a query string given as a string argument in Python 2.
We use `force_str` and ``force_text`` for handling Python 2 and Python 3
different behaviours.

This regression was introduced in b5eaa0d while fixing Python 3 support

paypal/base.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.utils.six.moves.urllib.parse import parse_qs
+from django.utils.encoding import force_text, force_str
 from django.utils.translation import ugettext_lazy as _
 
 from django.db import models
@@ -43,8 +44,8 @@ class ResponseModel(models.Model):
 
     @property
     def context(self):
-        return parse_qs(self.raw_response)
+        return parse_qs(force_str(self.raw_response))
 
     def value(self, key, default=None):
         ctx = self.context
-        return ctx[key][0] if key in ctx else default
+        return force_text(ctx[key][0]) if key in ctx else default

paypal/gateway.py
@@ -3,6 +3,7 @@ import requests
 import time
 
 from django.utils.http import urlencode
+from django.utils.encoding import force_text, force_str
 from django.utils.six.moves.urllib.parse import parse_qs
 
 from paypal import exceptions
@@ -26,8 +27,9 @@ def post(url, params):
 
     # Convert response into a simple key-value format
     pairs = {}
-    for key, values in parse_qs(response.text).items():
-        pairs[key] = values[0]
+    for key, values in parse_qs(force_str(response.text)).items():
+        pairs[force_text(key)] = force_text(values[0])
+
 
     # Add audit information
     pairs['_raw_request'] = payload