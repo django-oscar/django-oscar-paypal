202ce3e7df0777b276a106cdddde3e1a3f11760f
Domenico Cosentino <d.cosentino@4sigma.it>
2017-03-08 10:49:50 +0100

    Removed deprecated urlpatterns for having compatibility with django 1.10

--- paypal/express/dashboard/app.py
+++ paypal/express/dashboard/app.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.admin.views.decorators import staff_member_required
 
 from oscar.core.application import Application
@@ -12,12 +12,12 @@ class ExpressDashboardApplication(Application):
     detail_view = views.TransactionDetailView
 
     def get_urls(self):
-        urlpatterns = patterns('',
+        urlpatterns = [
             url(r'^transactions/$', self.list_view.as_view(),
                 name='paypal-express-list'),
             url(r'^transactions/(?P<pk>\d+)/$', self.detail_view.as_view(),
                 name='paypal-express-detail'),
-        )
+        ]
         return self.post_process_urls(urlpatterns)
 
     def get_url_decorator(self, url_name):
