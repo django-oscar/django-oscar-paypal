e6bb9a191d76113aabd18d83b1932cb95dd35e71
Alexander Gaevsky <sasha@sasha0.ru>
2020-09-25 13:28:36 +0300

    Remove Python 3.5 support, since it is reaching its end of life.

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         'Operating System :: Unix',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Other/Nonlisted Topic'],
