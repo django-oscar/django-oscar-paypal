94090c6be707c5d8700a1e5f95745ae17de91fb3
Basil Dubyk <samitnuk.work@gmail.com>
2020-10-07 19:56:08 +0300

    Modify urls configuration to keep all tests for `paypal.express`

--- tests/urls.py
+++ tests/urls.py
@@ -2,7 +2,9 @@ from django.apps import apps
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
-from paypal.express_checkout.urls import base_patterns, buyer_pays_on_paypal_patterns, buyer_pays_on_website_patterns
+from paypal.express.urls import base_patterns, buyer_pays_on_paypal_patterns, buyer_pays_on_website_patterns
+from paypal.express_checkout import urls
+
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -11,5 +13,10 @@ urlpatterns += i18n_patterns(
     path('checkout/paypal/', include(base_patterns)),
     path('checkout/paypal/option-paypal/', include(buyer_pays_on_paypal_patterns)),
     path('checkout/paypal/option-website/', include(buyer_pays_on_website_patterns)),
+
+    path('checkout/paypal/express-checkout/', include(urls.base_patterns)),
+    path('checkout/paypal/express-checkout/option-paypal/', include(urls.buyer_pays_on_paypal_patterns)),
+    path('checkout/paypal/express-checkout/option-website/', include(urls.buyer_pays_on_website_patterns)),
+
     path('', include(apps.get_app_config("oscar").urls[0])),
 )
